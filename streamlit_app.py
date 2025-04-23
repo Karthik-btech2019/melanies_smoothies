ceb81f07193c5d59e3de1aaa219996fb12450f21
Karthikeyan K R <karthi.btech2019@gmail.com>
2025-04-23 16:41:15 +0100

    Update streamlit_app.py
    Connection Changes at cnx

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,14 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
-cnx = st.connection("snowflake")
+cnx = st.connection("snowflake", type="snowflake", 
+account = "IEOOUBW-WSB80397"
+user = "KARTHIKEYAN06"
+password = "Karthikeyan@2002"
+role = "SYSADMIN"
+warehouse = "COMPUTE_WH"
+database = "SMOOTHIES"
+schema = "PUBLIC")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
